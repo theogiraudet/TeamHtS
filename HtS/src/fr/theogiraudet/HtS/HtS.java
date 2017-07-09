e5823bfc28ac68342a872064b9ecd5c3b669e4eb
BuildTools <unconfigured@null.spigotmc.org>
2017-07-09 18:50:57 +0200

    rt

--- HtS/src/fr/theogiraudet/HtS/HtS.java
+++ HtS/src/fr/theogiraudet/HtS/HtS.java
@@ -88,8 +88,6 @@ public class HtS extends JavaPlugin {
 		getCommand("players").setExecutor(new Commands(this));
 		getCommand("random").setExecutor(new Commands(this));
 		
-		System.out.println("Démarrage d'HtS.");
-		
 		
 	}
 
