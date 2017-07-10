cfac54bd4a8777cb1b8ccc9edd292ef4ac3412f4
ikbrunel <ikbrunel.coding@mail.com>
2017-07-10 20:19:47 +0200

    mobfriend fixed

--- HtS/src/fr/theogiraudet/HtS/Event/MobFriendEvent.java
+++ HtS/src/fr/theogiraudet/HtS/Event/MobFriendEvent.java
@@ -3,21 +3,22 @@ package fr.theogiraudet.HtS.Event;
 import java.util.ArrayList;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import fr.theogiraudet.HtS.HtS;
 import fr.theogiraudet.HtS.Objects.ItemStackManager;
 import fr.theogiraudet.HtS.Objects.Randomizer;
-import fr.theogiraudet.HtS.Objects.Team;
-import fr.theogiraudet.HtS.Objects.SpawnEntity;
 import net.md_5.bungee.api.ChatColor;
 
 public class MobFriendEvent implements Listener{
@@ -38,30 +39,27 @@ public class MobFriendEvent implements Listener{
 		}	
 	}
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void onMobSpawn(EntitySpawnEvent e){
-		if(e.getEntity() instanceof Monster){
-			System.out.println(e.getEntity().getCustomName());
-			for(Team t : main.getTeams())
-				if(t.getTeamName() == e.getEntity().getName().split(",")[0]){
-					SpawnEntity.summon(e.getEntityType(),e.getEntity().getWorld(), e.getLocation(), e.getEntity().getCustomName().split(",")[0],false);
-			}
+	public void onMobSpawn(PlayerInteractEvent e){
+		if(e.getPlayer().getItemInHand().getType() == Material.MONSTER_EGG && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			e.setCancelled(true);
+			Entity mob = e.getPlayer().getWorld().spawnEntity(e.getPlayer().getLocation(), EntityType.fromName(e.getItem().getItemMeta().getDisplayName().split(": ")[1]));
+			mob.setCustomNameVisible(false);
+			mob.setCustomName(main.getTeam(e.getPlayer()).getTeamName());
+			e.getPlayer().getItemInHand().setAmount(e.getPlayer().getItemInHand().getAmount()-1);
 		}
 	}
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onMobDeath(EntityDeathEvent e){
-		System.out.println("death");
-		if(e.getEntity() instanceof Monster){
-			System.out.println("dectect monster");
-			if(Randomizer.RandRate(80)){
-				System.out.println("over rand");
+		if(e.getEntity().getCustomName() == null && (e.getEntity() instanceof Monster || e.getEntityType() == EntityType.SLIME)){
+			if(Randomizer.RandRate(10)){
 				if(e.getEntity().getKiller() instanceof Player){
-					System.out.println("killer");
-					Player p = e.getEntity().getKiller();
-					
 					ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-					drops.add(new ItemStackManager(Material.MONSTER_EGG, (short) e.getEntity().getEntityId(), 1, ChatColor.GREEN + main.getTeam(p).getTeamName() + ", appel à un ami", "Fait apparaître un monstre qui combattra à vos côtés (il ne vous suivera pas).").getItemStack());
+					drops.add(new ItemStackManager(Material.MONSTER_EGG, e.getEntityType().getTypeId(), 1, ChatColor.GREEN + main.getTeam(e.getEntity().getKiller()).getTeamName() + " a un appel à un : " + e.getEntity().getName(), "Fait apparaître un monstre qui combattra à vos côtés (il ne vous suivera pas).").getItemStack());
+					e.getDrops().addAll(drops);
 				}
 			}	
 		}
