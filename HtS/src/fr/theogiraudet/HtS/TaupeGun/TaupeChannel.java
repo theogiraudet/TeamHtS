10519eb536d17c55a53a43c01febf8b5d02f6bae
theogiraudet <theo.giraudet@orange.fr>
2017-07-19 16:32:59 +0200

    Shulker fixe

--- HtS/src/fr/theogiraudet/HtS/TaupeGun/TaupeChannel.java
+++ HtS/src/fr/theogiraudet/HtS/TaupeGun/TaupeChannel.java
@@ -25,10 +25,10 @@ public class TaupeChannel implements Listener {
 			for (UUID uuid : main.taupeGun.getTaupes()) {
 				Bukkit.getPlayer(uuid).sendMessage("§8[§4" + e.getPlayer().getName() + "§8] " + messageS);
 			}
+			e.setCancelled(true);
 		} else if(main.isTaupeState(ModState.PRORAND) && !main.taupeGun.getTaupes().contains(e.getPlayer().getUniqueId()) && e.getMessage().substring(0, 1) == "!") {
 			e.getPlayer().sendMessage("§4Vous n'êtes pas une taupe !");
+			e.setCancelled(true);
 		}
-
-		e.setCancelled(true);
 	}
 }
