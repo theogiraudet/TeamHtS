cf7039b5e672598cc392dba0291a0b8d57c8b069
ikbrunel <ikbrunel.coding@mail.com>
2017-07-21 22:48:51 +0200

    Update

--- HtS/src/fr/theogiraudet/HtS/LootTables/LootTables.java
+++ HtS/src/fr/theogiraudet/HtS/LootTables/LootTables.java
@@ -18,11 +18,14 @@ public class LootTables {
 	public static void loadLootTables() {
 		for(World w : Bukkit.getWorlds()) {if(w.getEnvironment() == Environment.NORMAL) { wdir = w.getName() + "/data/loot_tables";}}
 		try {
+			//Entities
 			FileExtractor.extractFile(lt + "bat.json", wdir + Edir);
 			FileExtractor.extractFile(lt + "ghast.json", wdir + Edir);
 			FileExtractor.extractFile(lt + "skeleton.json", wdir + Edir);
 			FileExtractor.extractFile(lt + "zombie.json", wdir + Edir);
+			//Gameplay
 			FileExtractor.extractFile(lt + "fishing.json", wdir + Gdir);
+			//Custom
 			FileExtractor.extractFile(lt + "coffre.json", wdir + Cdir);
 			FileExtractor.extractFile(lt + "crate.json", wdir + Cdir);
 			
